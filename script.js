8f96001bc335882e40e270025bb638e6ec6798b4
danishjamadar5 <117918682+danishjamadar5@users.noreply.github.com>
2023-05-16 17:14:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function threeSum(arr, target) {
-// write your code here
-  
-}
+// function threeSum(arr, target) {
 
-module.exports = threeSum;
+
+// write your code here
+	function decimalToBinary(num){
+  if(num===0) return '0';
+	const binary=' ';
+
+	while(n>0){
+		binary=(num%2)+ binary;
+		num=math.floo(num/2);
+	}
+	return binary;
+}
+console.log(decimalToBinary);
+
+
+// module.exports = threeSum;
+
+
